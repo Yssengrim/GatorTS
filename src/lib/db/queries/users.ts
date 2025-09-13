8098ec57dbc37837ae5600567c8dd961bb2aaf03
Yssengrim <eisengrin@gmail.com>
2025-09-13 18:45:52 -0500

    Database Chapter Complete Commit

---
+++ src/lib/db/queries/users.ts
@@ -0,0 +1,22 @@
+import { db } from "..";
+import { users } from "../schema";
+import { eq } from "drizzle-orm";
+
+
+export async function createUser(name: string) {
+  const [result] = await db.insert(users).values({ name: name }).returning();
+  return result;
+}
+
+export async function getUserByName(name: string) {
+  const [row] = await db.select().from(users).where(eq(users.name, name));
+  return row; 
+}
+
+export async function resetUsers() {
+  await db.delete(users);
+}
+
+export async function getUsers() {
+  return db.select().from(users);
+}
